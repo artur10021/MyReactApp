32a8d9bfb57aa33da30f75312d807907a214647b
Artur <arturzelkovskiy@gmail.com>
2021-11-09 23:41:06 +0300

    changed/added posts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,10 +1,18 @@
 import classes from "./MyPosts.module.css"
+import Post from "./Post/Post"
 function MyPosts(){
     return(
         <div className={classes.items}>
              <div className={classes.items}> avatar</div>
-             <div>my posts</div>
-             <div>posts</div>
+             <div>my posts:</div>
+             <textarea>text a post</textarea>
+             <button>Add post</button>
+             <Post postMassege="Hi, how are you?" likes="15"/>
+             <Post postMassege="I, hope you will enjoy my proect!" likes="20"/>
+             <Post/>
+             <Post/>
+
+             
         </div>
     );
 }
