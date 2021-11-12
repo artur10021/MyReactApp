8c7026e1301bf52c4b18b0fda04efbdd7ee02986
Artur <arturzelkovskiy@gmail.com>
2021-11-12 23:14:05 +0300

    added NavLinks

--- src/components/Navigator/Navigator.jsx
+++ src/components/Navigator/Navigator.jsx
@@ -1,11 +1,20 @@
+import { NavLink } from "react-router-dom";
 import classes from "./Navigator.module.css"
 function Navigator() {
     return (
         <nav className={classes.navigator}>
-            <div><a href="/profile">Profile</a> </div>
-            <div><a href="/dialogues">Masages</a> </div>
-            <div><a href="/news">News</a> </div>
-            <div><a href="/setings">Setings</a> </div>
+            <div className={classes.navLinks}>
+                <NavLink to="/profile" className={(navData)=>navData.isActive ? classes.activeLink : ''}>Profile</NavLink>
+            </div>
+            <div className={classes.navLinks}>
+                <NavLink to="/dialogues" className={(navData)=>navData.isActive ? classes.activeLink : ''}>Masages</NavLink>
+            </div>
+            <div className={classes.navLinks}>
+                <NavLink to="/news" className={(navData)=>navData.isActive ? classes.activeLink : ''}>News</NavLink>
+            </div>
+            <div className={classes.navLinks}>
+                <NavLink to="/setings" className={(navData)=>navData.isActive ? classes.activeLink : ''}>Setings</NavLink>
+            </div>
         </nav>
     )
 }
